32b6715ccadb1f269a5f1ab33995f16ebbe1bdbd
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-12 07:38:21 +0600

    create random string for token

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -36,7 +36,23 @@ utilities.hash = (str) => {
 };
 
 // hash string
-utilities.createRandomString = (strLength) => 'haldjalkajdslkfj';
+utilities.createRandomString = (strLength) => {
+  let length = strLength;
+  length = typeof strLength === 'number' && strLength > 0 ? strLength : false;
+
+  if (length) {
+    const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    let output = '';
+    for (let i = 1; i <= length; i += 1) {
+      const randomCharacter = possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length),
+      );
+      output += randomCharacter;
+    }
+    return output;
+  }
+  return false;
+};
 
 // export module
 module.exports = utilities;
