64ed84859c0f99ac578b8e6876e71f4ab0860fab
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-14 06:38:31 +0600

    put token done

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -91,7 +91,43 @@ handler._token.get = (requestProperties, callback) => {
 };
 
 // Authentication
-handler._token.put = (requestProperties, callback) => {};
+handler._token.put = (requestProperties, callback) => {
+  const id =
+    typeof requestProperties.body.id === 'string' && requestProperties.body.id.trim().length > 20
+      ? requestProperties.body.id
+      : false;
+
+  const extend = !!(
+    typeof requestProperties.body.extend === 'string' && requestProperties.body.extend === true
+  );
+
+  if (id && extend) {
+    data.read('tokens', id, (err1, tokenData) => {
+      const tokenObject = parseJSON(tokenData);
+      if (tokenObject.expires > Date.now()) {
+        tokenObject.expires = Date.now() + 60 * 60 * 1000;
+        // store the updated token
+        data.update('tokens', id, tokenObject, (err2) => {
+          if (!err2) {
+            callback(200);
+          } else {
+            callback(500, {
+              error: 'There was a server side error',
+            });
+          }
+        });
+      } else {
+        callback(400, {
+          error: 'Token already expired!',
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: 'There was a problem in your request',
+    });
+  }
+};
 
 // Authentication
 handler._token.delete = (requestProperties, callback) => {};
