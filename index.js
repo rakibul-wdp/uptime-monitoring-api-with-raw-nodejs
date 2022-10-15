15462e5c5fb01119c9d956ea4a1f31089340c1de
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-15 11:08:21 +0600

    change index file

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-/**
- * Title: Uptime Monitoring Application
- * Description: A RESTFul API monitor up or down time of user defined links
+/*
+ * Title: Project Initial file
+ * Description: Initial file to start the node server and workers
  * Author: Md. Rakibul Islam
- * Date: 23/09/2022
+ * Date: 15/10/2022
+ *
  */
-
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const environment = require('./helpers/environments');
-const data = require('./lib/data');
-const { sendTwilioSms } = require('./helpers/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
-// @TODO remove later
-sendTwilioSms('01911111111', 'Hello world', (err) => {
-  console.log('this is the error', err);
-});
-
-// configuration
-app.config = {
-  port: 3000,
+app.init = () => {
+  // start the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-// testing file system
-// @TODO: delete after some time
-data.create('test', 'newFile', { name: 'Bangladesh', language: 'Bangla' }, (err) => {
-  console.log(err);
-});
+app.init();
 
-// create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(environment.port, () => {
-    console.log(`listening to port ${environment.port}`);
-  });
-};
-
-// handle Request Response
-app.handleReqRes = handleReqRes;
-
-// start the server
-app.createServer();
+// export the app
+module.exports = app;
