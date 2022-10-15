776db8691942f67db5e51bc3fbd9ae6b9db5b7a5
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-15 09:23:43 +0600

    create check post

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -7,9 +7,10 @@
 
 // dependencies
 const data = require('../../lib/data');
-const { hash } = require('../../helpers/utilities');
+const { hash, createRandomString } = require('../../helpers/utilities');
 const { parseJSON } = require('../../helpers/utilities');
 const { tokenHandler } = require('./tokenHandler');
+const {maxChecks} = require('../../helpers/environments');
 
 // module scaffolding
 const handler = {};
@@ -52,6 +53,85 @@ handler._check.post = (requestProperties, callback) => {
   && requestProperties.body.timeoutSeconds <= 5
     ? requestProperties.body.successCodes
     : false;
+
+  if (protocol && url && method && successCodes && timeoutSeconds) {
+    let token = typeof requestProperties.headersObject.token === 'string' ? requestProperties.headersObject.token : false;
+
+    // lookup the user phone by reading the token
+    data.read('tokens', token, (err1, tokenData) => {
+      if (!err1 && tokenData) {
+        let userPhone = parseJSON(tokenData).phone;
+        // lookup the user data
+        data.read('users', userPhone, (err2, userData) => {
+          if (!err2 && userData) {
+            tokenHandler._token.verify(token, userPhone, (tokenIsValid) => {
+              if (tokenIsValid) {
+                let userObject = parseJSON(userData);
+                let userChecks = typeof(userObject.checks) === 'object' && userObject.checks instanceof Array ? userObject.checks : [];
+                
+                if (userChecks.length < maxChecks) {
+                  let checkId = createRandomString(20);
+                  let checkObject = {
+                    'id': checkId,
+                    userPhone,
+                    protocol,
+                    url,
+                    method,
+                    successCodes,
+                    timeoutSeconds,
+                  };
+                  // save the object
+                  data.create('checks', checkId, checkObject, (err3) => {
+                    if (!err3) {
+                      // add check id to the user's object
+                      userObject.checks = userChecks;
+                      userObject.checks.push(checkId);
+
+                      // save the new user data
+                      data.update('users', userPhone, userObject, (err4) => {
+                        if (!err4) {
+                          // return the data about the new check
+                          callback(200, checkObject);
+                        } else {
+                          callback(500, {
+                            error: 'There was a problem in the server side!'
+                          })
+                        }
+                      })
+                    } else {
+                      callback(500, {
+                        error: 'There was a problem in the server side!'
+                      })
+                    }
+                  })
+                } else {
+                  callback(401, {
+                    error: 'User has already reached max check limit',
+                  })
+                }
+              } else {
+                callback(403, {
+                  error: 'Authentication problem',
+                }
+              }
+            })
+          } else {
+            callback(403, {
+              error: 'Use not found',
+            })
+          }
+        })
+      } else (
+        callback(403, {
+          error: 'Authentication problem',
+        })
+      })
+    })
+  } else {
+    callback(400, {
+      error: 'You have a problem in your request',
+    })
+  }
 };
 
 handler._check.get = (requestProperties, callback) => {};
--- helpers/environments.js
+++ helpers/environments.js
@@ -14,19 +14,23 @@ environments.staging = {
   port: 3000,
   envName: 'staging',
   secretKey: 'hkalsdkdfalsdkfjlasdfsl',
+  maxChecks: 5,
 };
 
 environments.production = {
   port: 5000,
   envName: 'production',
   secretKey: 'hkalslksdjflslieowasdfsl',
+  maxChecks: 5,
 };
 
 // determine which environment was passed
-const currentEnvironment =  typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+const currentEnvironment =
+  typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
 
 // export corresponding environment object
-const environmentToExport =  typeof environments[currentEnvironment] === 'object'
+const environmentToExport =
+  typeof environments[currentEnvironment] === 'object'
     ? environments[currentEnvironment]
     : environments.staging;
 
